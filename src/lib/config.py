7ce2aa75d90fdb5762b2eb2a455f6a2389fbaac3
patrickab <patrick.a.bitzer@gmail.com>
2025-08-02 17:30:12 +0200

    feat: Reactor Geometry - Introduce 2d toroidal field coils expressed in XYZ

--- src/lib/config.py
+++ src/lib/config.py
@@ -1,5 +1,8 @@
 """Configuration module for this repository."""
 
+import numpy as np
+
+
 class Filepaths:
     """
     Configuration class for file paths used in the application.
@@ -8,3 +11,16 @@ class Filepaths:
     ROOT = "~/fusion-simulation"
     OUTPUT_DIR = "data"
     REACTOR_POLYGONIAL_MESH = "plasma_surface.ply"
+    REACTOR_TOROIDAL_FIELD_COILS = "toroidal_field_coils.ply"
+
+
+class RotationalAngles:
+    """
+    Configuration class for rotational angles used in the application.
+    """
+
+    n_phi = 360  # Number of points in toroidal direction
+    n_theta = 360  # Number of points in poloidal direction
+
+    PHI = np.linspace(0, 2 * np.pi, n_phi)  # Azimuthal angle in radians
+    THETA = np.linspace(0, 2 * np.pi, n_theta)  # Polar angle in radians
