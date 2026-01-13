c62874c2b20916d21830cc818bec6ee4b1680587
patrickab <patrick.a.bitzer@gmail.com>
2026-01-13 17:54:36 +0100

    fix: Provide filepath for network serialization

--- src/lib/config.py
+++ src/lib/config.py
@@ -18,6 +18,7 @@ class Filepaths:
     PLASMA_SURFACE = OUTPUT_DIR / "plasma_surface.ply"
     TOROIDAL_COILS = OUTPUT_DIR / "toroidal_field_coils.ply"
     TOROIDAL_COIL_3D_DIR = OUTPUT_DIR / "toroidal_coils_3d"
+    PINN_PATH = OUTPUT_DIR / "pinn_model"
 
 
 class BaseModel:
